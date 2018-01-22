148a3b086f591dcf2ff8e9cc296014ecdf5f4f52
Thomas Lowry <minilao94@yahoo.com>
2018-01-21 23:14:25 -0700

    added server & ran build

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+app.use(express.static(`${__dirname}/../build`));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+app.listen(3006, () => console.log('portfolio running on port 3006'))
